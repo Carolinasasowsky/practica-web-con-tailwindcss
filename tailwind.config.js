10c21058e11630b54b92257e6d712ce46be97bdb
Carolina Sasowsky <mc.sasowsky@gmail.com>
2024-02-08 21:17:12 -0300

    se inicio con el contenedor principal y se estilo con tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+tailwind.config = {
+  theme: {
+    screens: {
+      sm: "600px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "875px",
+      // => @media (min-width: 960px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1440px) { ... }
+      },
+  },
+};
